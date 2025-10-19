8d699dc26bfc126f77544c4c88fb098421ac8e38
nghialt3670 <nghialt3670@gmail.com>
2025-10-19 20:06:39 +0700

    feat: add fabric filters

---
+++ core/chat2edit/models/fabric/filters/__init__.py
@@ -0,0 +1,17 @@
+from core.chat2edit.models.fabric.filters.base_filter import BaseFilter
+from core.chat2edit.models.fabric.filters.black_white_filter import BlackWhiteFilter
+from core.chat2edit.models.fabric.filters.blur_filter import BlurFilter
+from core.chat2edit.models.fabric.filters.brightness_filter import BrightnessFilter
+from core.chat2edit.models.fabric.filters.contrast_filter import ContrastFilter
+from core.chat2edit.models.fabric.filters.invert_filter import InvertFilter
+from core.chat2edit.models.fabric.filters.saturation_filter import SaturationFilter
+
+__all__ = [
+    "BaseFilter",
+    "BlackWhiteFilter",
+    "BlurFilter",
+    "BrightnessFilter",
+    "ContrastFilter",
+    "InvertFilter",
+    "SaturationFilter",
+]
